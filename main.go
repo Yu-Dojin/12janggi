9d7466385eb828880023a1f7bc5ac128ba5f0bd8
dgjeon <jdk0987@gmail.com>
2020-03-12 10:26:23 +0900

    modi new import file

--- main.go
+++ main.go
@@ -33,17 +33,6 @@ const (
 	TeamRed
 )
 
-const (
-	ScreenWidth  = 480
-	ScreenHeight = 362
-	GimulStartX  = 20
-	GimulStartY  = 23
-	GridWidth    = 116
-	GridHeight   = 116
-	BoardWidth   = 4
-	BoardHeight  = 3
-)
-
 var (
 	board       [BoardWidth][BoardHeight]GimulType
 	bgimg       *ebiten.Image
--- scenes/gamescene.go
+++ scenes/gamescene.go
@@ -1,14 +1,87 @@
 package scenes
 
-import "github.com/hajimehoshi/ebiten"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
+)
 
 type GameScene struct {
+	bgimg    *ebiten.Image
+	gameover bool
 }
 
 func (g *GameScene) Startup() {
-
+	g.gameover = false
+	var err error
+	g.bgimg, _, err = ebitenutil.NewImageFromFile("./images/bgimg.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error : %v", err)
+	}
 }
 
-func (g *GameScene) Update(*ebiten.Image) error {
+func (g *GameScene) Update(screen *ebiten.Image) error {
+
+	screen.DrawImage(g.bgimg, nil)
+	if g.gameover {
+		return nil
+	}
+
+	// Input handling
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		i, j := x/GridWidth, y/GridHeight
+
+		if selected {
+			if i == selectedX && j == selectedY {
+				selected = false
+			} else {
+				//Move
+				move(selectedX, selectedY, i, j)
+			}
+		} else {
+			if board[i][j] != GimulTypeNone && currentTeam == GetTeamType(board[i][j]) {
+				selected = true
+				selectedX, selectedY = i, j
+			}
+
+		}
+	}
+
+	// Draw gimuls
+	for i := 0; i < BoardWidth; i++ {
+		for j := 0; j < BoardHeight; j++ {
+
+			opts := &ebiten.DrawImageOptions{}
+			opts.GeoM.Translate(float64(GimulStartX+GridWidth*i), float64(GimulStartY+GridHeight*j))
+
+			switch board[i][j] {
+			case GimulTypeGreenWang:
+				screen.DrawImage(gimulImgs[GimulTypeGreenWang], opts)
+			case GimulTypeGreenJa:
+				screen.DrawImage(gimulImgs[GimulTypeGreenJa], opts)
+			case GimulTypeGreenJang:
+				screen.DrawImage(gimulImgs[GimulTypeGreenJang], opts)
+			case GimulTypeGreenSang:
+				screen.DrawImage(gimulImgs[GimulTypeGreenSang], opts)
+			case GimulTypeRedWang:
+				screen.DrawImage(gimulImgs[GimulTypeRedWang], opts)
+			case GimulTypeRedJa:
+				screen.DrawImage(gimulImgs[GimulTypeRedJa], opts)
+			case GimulTypeRedJang:
+				screen.DrawImage(gimulImgs[GimulTypeRedJang], opts)
+			case GimulTypeRedSang:
+				screen.DrawImage(gimulImgs[GimulTypeRedSang], opts)
+			}
+		}
+	}
+
+	if selected {
+		opts := &ebiten.DrawImageOptions{}
+		opts.GeoM.Translate(float64(GimulStartX+GridWidth*selectedX), float64(GimulStartY+GridHeight*selectedY))
+		screen.DrawImage(selectedImg, opts)
+	}
 	return nil
 }
