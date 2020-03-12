7f4a56d0383e0386e0160f6154ccf0f3f964fdc7
dgjeon <jdk0987@gmail.com>
2020-03-12 10:15:17 +0900

    modi import

---
+++ scenes/gamescene.go
@@ -0,0 +1,12 @@
+package scenes
+
+type GameScene struct {
+}
+
+func (g *GameScene) Startup() {
+
+}
+
+func (g *GameScene) Update(*ebiten.Image) error {
+	return nil
+}
--- scenes/startscene.go
+++ scenes/startscene.go
@@ -3,8 +3,10 @@ package scenes
 import (
 	"log"
 
+	"github.com/Yu-Dojin/12janggi/scenemanager"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
 type StartScene struct {
@@ -21,5 +23,9 @@ func (s *StartScene) Startup() {
 
 func (s *StartScene) Update(screen *ebiten.Image) error {
 	screen.DrawImage(s.startImg, nil)
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		// Set GameScene
+		scenemanager.SetScene(&GameScene{})
+	}
 	return nil
 }
